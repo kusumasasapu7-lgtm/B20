8fb846da80b9e7b92c342778b1c06a6878c3463f
kusumasasapu7-lgtm <kusumasasapu7@gmail.com>
2026-05-10 20:50:42 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,14 @@
 const firebaseConfig = {
+  apiKey: "AIzaSyB05QZtH6NwKnALc6nJjItIMvdFh7l2fJI",
+  authDomain: "mindsync-cc3c2.firebaseapp.com",
+  projectId: "mindsync-cc3c2",
+  storageBucket: "mindsync-cc3c2.firebasestorage.app",
+  messagingSenderId: "1010342912964",
+  appId: "1:1010342912964:web:cafa8698627cd008adba91",
+  measurementId: "G-6RCCNX1VRY"
+};
 
-    apiKey: "AIzaSyB05QZtH6NwKnALc6nJjItIMvdFh7l2fJI",
+firebase.initializeApp(firebaseConfig);
 
-    authDomain: "mindsync-cc3c2.firebaseapp.com",
-
-    projectId: "mindsync-cc3c2",
-
-    storageBucket: "mindsync-cc3c2.firebasestorage.app",
-
-    messagingSenderId: "1010342912964",
-
-    appId: "1:1010342912964:web:cafa8698627cd008adba91",
-
-    measurementId: "G-6RCCNX1VRY"
-
-  };
-
-
-  // Initialize Firebase
-
-  const app = initializeApp(firebaseConfig);
-
-  const analytics = getAnalytics(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
